7f9988927260a06031884026aaa1c54ab583ab3d
mc4rvalho <mcarvalho093@gmail.com>
2025-12-10 12:05:45 -0300

    Criando ContaController e atualizando Menu

---
+++ src/controller/ContaController.ts
@@ -0,0 +1,46 @@
+import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+import { colors } from "../util/Colors";
+
+export class ContaController implements ContaRepository {
+
+  private listaContas: Array<Conta> = new Array<Conta>();
+  numero: number = 0;
+
+  listarTodas(): void {
+    for (let conta of this.listaContas) {
+      conta.visualizar();
+    };
+  }
+
+  cadastrar(conta: Conta): void {
+    this.listaContas.push(conta);
+    console.log(colors.fg.green, "\nA Conta número: " + conta.numero + " foi criada com sucesso!", colors.reset);
+  }
+
+  public gerarNumero(): number {
+    return ++this.numero;
+  }
+
+  procurarPorNumero(numero: number): void {
+    throw new Error("Method not implemented.");
+  }
+  atualizar(conta: Conta): void {
+    throw new Error("Method not implemented.");
+  }
+  deletar(numero: number): void {
+    throw new Error("Method not implemented.");
+  }
+  sacar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+  depositar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+  transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+
+
+
+}
