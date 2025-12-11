15607415aba9c0fc2d91a566d56a45a730b7486a
mc4rvalho <mcarvalho093@gmail.com>
2025-12-11 08:50:30 -0300

    Implementando sacar, depositar e transferência e colocando para funcionar no Menu

--- Menu.ts
+++ Menu.ts
@@ -10,7 +10,7 @@ export function main() {
     let contas: ContaController = new ContaController();
 
     // Variáveis Auxiliares
-    let opcao, numero, agencia, tipo, saldo, limite, aniversario: number;
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario, valor, numeroDestino: number;
     let titular: string;
     const tiposContas = ['Conta Corrente', 'Conta Poupanca']
 
@@ -161,9 +161,9 @@ export function main() {
                 console.log(colors.fg.whitestrong,
                     "\n\nApagar uma Conta\n\n", colors.reset);
 
-                    console.log("Digite o número da Conta: ");
-                    numero = readlinesync.questionInt("");
-                    contas.deletar(numero);
+                console.log("Digite o número da Conta: ");
+                numero = readlinesync.questionInt("");
+                contas.deletar(numero);
 
                 keyPress()
                 break;
@@ -171,17 +171,44 @@ export function main() {
                 console.log(colors.fg.whitestrong,
                     "\n\nSaque\n\n", colors.reset);
 
+                console.log("Digite o número da Conta: ");
+                numero = readlinesync.questionInt("");
+
+                console.log("Digite o valor do Saque (R$): ");
+                valor = readlinesync.questionInt("");
+
+                contas.sacar(numero, valor);
+
                 keyPress()
                 break;
             case 7:
                 console.log(colors.fg.whitestrong,
                     "\n\nDepósito\n\n", colors.reset);
 
+                console.log("Digite o número da Conta: ");
+                numero = readlinesync.questionInt("");
+
+                console.log("Digite o valor do Depósito (R$): ");
+                valor = readlinesync.questionInt("");
+
+                contas.depositar(numero, valor);
+
                 keyPress()
                 break;
             case 8:
                 console.log(colors.fg.whitestrong,
                     "\n\nTransferência entre Contas\n\n", colors.reset);
+
+                console.log("Digite o número da Conta de Origem: ");
+                numero = readlinesync.questionInt("");
+
+                console.log("Digite o número da Conta de Destino: ");
+                numeroDestino = readlinesync.questionInt("");
+
+                console.log("Digite o valor do Depósito (R$): ");
+                valor = readlinesync.questionInt("");
+
+                contas.transferir(numero, numeroDestino, valor)
 
                 keyPress()
                 break;
--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -30,7 +30,7 @@ export class ContaController implements ContaRepository {
     } else
       console.log(colors.fg.red, "\nA Conta número: " + numero + " não foi encontrada!", colors.reset);
   }
-  
+
   atualizar(conta: Conta): void {
     let buscaConta = this.buscarNoArray(conta.numero);
 
@@ -52,13 +52,37 @@ export class ContaController implements ContaRepository {
   }
 
   sacar(numero: number, valor: number): void {
-    throw new Error("Method not implemented.");
+    let conta = this.buscarNoArray(numero);
+
+    if (conta != null) {
+
+      if (conta.sacar(valor) == true)
+        console.log(colors.fg.green, "\nO Saque na Conta número: " + numero + " foi efetuado com sucesso!", colors.reset);
+    } else
+      console.log(colors.fg.green, "\nA Conta número: " + numero + " não foi encontrada!", colors.reset);
   }
+
   depositar(numero: number, valor: number): void {
-    throw new Error("Method not implemented.");
+    let conta = this.buscarNoArray(numero);
+
+    if (conta != null) {
+      conta.depositar(valor);
+      console.log(colors.fg.green, "\nO Depósito na Conta número: " + numero + " foi efetuado com sucesso!", colors.reset);
+    } else
+      console.log(colors.fg.red, "\nA Conta número: " + numero + " não foi encontrada!", colors.reset);
   }
+
   transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
-    throw new Error("Method not implemented.");
+    let contaOrigem = this.buscarNoArray(numeroOrigem);
+    let contaDestino = this.buscarNoArray(numeroDestino);
+
+    if (contaOrigem != null && contaDestino != null) {
+      if (contaOrigem.sacar(valor) == true) {
+        contaDestino.depositar(valor);
+        console.log(colors.fg.green, "A Transferência da Conta número: " + numeroOrigem + " para a Conta número: " + numeroDestino + " foi efetuada com sucesso!", colors.reset);
+      }
+    } else
+      console.log(colors.fg.red, "\nA Conta número: " + numeroOrigem + " e/ou a Conta número: " + numeroDestino + " não foram encontradas!", colors.reset);
   }
 
   //Checa se uma Conta existe
